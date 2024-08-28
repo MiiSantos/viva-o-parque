0716701f1c9c0054f8e78dee21f29177cd1f2ac4
Miria Santos <73804490+MiiSantos@users.noreply.github.com>
2024-08-27 22:19:26 -0300

    Delete directory

--- viva-o-parque.api/src/main/java/api/viva_o_parque/repositories/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package api.viva_o_parque.repositories;
-
-import api.viva_o_parque.entities.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
